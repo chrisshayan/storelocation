0f09e073ee8b47e9d5ffaaad10a436e7f2d0387c
tankhuu <jackiekhuu.work@gmail.com>
2019-10-03 11:36:37 +0700

    clone backend

--- README.md
+++ README.md
@@ -4,20 +4,26 @@ The Experimental Project in Mekong Capital
 
 ## Requirements
 ### FrontEnd
-nodejs version: 10.16.3
-angular cli version: 8.3.4
+nodejs version: 10.16.3 `https://nodejs.org/en/download/`
+
+angular cli version: 8.3.4 `npm install -g @angular/cli`
 
 ### Backend
-python version: 3.7
-flask version: 
+python version: 3.7 `https://www.python.org/downloads/`
+pipenv `https://github.com/pypa/pipenv`
+flask version: 1.1.1 `https://palletsprojects.com/p/flask/`
 
 ## Development server
 ### FrontEnd
 Go to project folder `cd frontend`
+
 Install requirements packages `npm install`
+
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
 ### Backend
 Go to project folder `cd backend`
+
 Install requirements packages `pipenv install`
+
 Run `pipenv shell && flask run` for a dev server. Navigate to `http://localhost:5000/`. The app will automatically reload if you change any of the source files.
---
+++ backend/flaskr/__init__.py
@@ -0,0 +1,60 @@
+from .collections.places import Places
+from .api.place import Place
+from flask_cors import CORS
+from flask import Flask, request, jsonify
+import os
+import json
+
+
+def create_app(test_config=None):
+    # Create and Configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    CORS(app)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.mongo')
+    )
+
+    if test_config is None:
+        # Load the instance config, if it exists, when not testing
+        app.config.from_pyfile('settings.py', silent=True)
+    else:
+        # Load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # initiate place
+    place = Place()
+
+    @app.route('/')
+    def index():
+        return 'Places backend!'
+
+    @app.route('/place/autocomplete', methods=['POST'])
+    def autocomplete():
+        predictions = []
+        input = request.get_json().get('input')
+        predictions = place.autocomplete(input)
+        if(predictions):
+            return jsonify(predictions)
+
+        return jsonify(predictions)
+
+    @app.route('/place/search', methods=['POST'])
+    def search():
+        result = {}
+        query = request.get_json().get('query')
+        radius = request.get_json().get('radius')
+
+        search_result = place.search(query=query, radius=radius)
+        if(search_result):
+            return jsonify(search_result)
+
+        return jsonify(result)
+
+    return app
