f16d924f84b1b4717d679eabcdbe00660a972416
tankhuu <jackiekhuu.work@gmail.com>
2019-10-17 10:10:32 +0700

    move domain to mekongcapital

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   gmapsPlaceRedirect: 'https://www.google.com/maps/place/?q=place_id:',
-  apiUrl: 'https://storelocation.precitavn.com/api/place'
+  apiUrl: 'https://storelocation.mekongcapital.com/api/place'
 };
