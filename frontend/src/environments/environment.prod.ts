b805fbcd5cc21f0d605fdb74b6249f3bde1b7ba7
Tan Khuu <tankhuu@Tans-iMac.net.fpt>
2019-11-13 23:26:14 +0700

    remove unused script

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  gmapsPlaceRedirect: 'https://www.google.com/maps/place/?q=place_id:',
-  apiUrl: 'https://storelocation.mekongcapital.com/api/place'
-};
