de0395702b50b5759c9c1d84f2602b605958a691
Tan Khuu <tankhuu@Tans-iMac.net.fpt>
2019-11-27 21:53:17 +0700

    function collect places: check features

--- utilities/functions/index.js
+++ utilities/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
+const rp = require('request-promise')
 
 admin.initializeApp()
 
@@ -8,5 +9,22 @@ exports.collectPlaces = functions.database.ref('places/conditions/{conditionsId}
     console.log('doc_id: ', context.params.conditionsId)
     console.log('snapshot', snapshot.val())
     console.log('call collect places with these conditions')
+
+    console.log('call backend')
+    const options = {
+      method: 'POST',
+      uri: '',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: { "query": "ChIJwd9IoKarNTERT3C7BsMeOjU", "radius": 50 },
+      json: true // Automatically parses the JSON string in the response
+    };
+
+    rp(options)
+      .then(res => console.log('response: ', res))
+      .catch(err => console.log('error: ', err));
+
     return snapshot.ref.update({ status: 'running' })
   })
