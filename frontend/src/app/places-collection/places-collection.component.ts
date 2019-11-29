7e52aa4b33af65d8a2cd470fe8860294dd2aa8c9
Tan Khuu <tankhuu@Tans-iMac.net.fpt>
2019-11-29 22:33:02 +0700

    transform places types summary

--- frontend/src/app/places-collection/places-collection.component.ts
+++ frontend/src/app/places-collection/places-collection.component.ts
@@ -1,16 +1,18 @@
+import { map } from 'rxjs/operators';
 import { PlacesCollectionService } from './../places-collection.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import _ from 'underscore'
 
 @Component({
-  selector: 'app-places-collection',
+  selector: 'places-collection',
   templateUrl: './places-collection.component.html',
   styleUrls: ['./places-collection.component.css']
 })
 export class PlacesCollectionComponent implements OnInit {
   collectionId: string
   doc
+  placesTypes = []
   // private collectionDoc: AngularFirestoreDocument
   // collection: Observable<>
 
@@ -20,10 +22,28 @@ export class PlacesCollectionComponent implements OnInit {
     this.route.paramMap.subscribe(params => {
       this.collectionId = params.get('id')
       if (!_.isEmpty(this.collectionId)) {
-        this.placesCollectionService.get(this.collectionId).subscribe(doc => this.doc = doc.data())
+        this.placesCollectionService.get(this.collectionId).subscribe(doc => {
+          this.doc = doc.data()
+          if (!_.isEmpty(this.doc)) {
+            if (!_.isEmpty(this.doc.places)) {
+              this.placesTypes = this.transformPlaces(this.doc.places)
+              console.log('placesTypes: ', JSON.stringify(this.placesTypes))
+            }
+          }
+        })
       }
     })
     console.log('collectionId: ', this.collectionId)
+  }
+
+  transformPlaces(places) {
+    return places.map(p => {
+      const id = !_.isEmpty(p.origin.id) ? p.origin.id : p.origin.coordinate
+      const name = p.origin.name || ''
+      const types = p.summary.types || {}
+
+      return { id, name, types }
+    })
   }
 
 }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,17 @@
 export const environment = {
   production: false,
   gmapsPlaceRedirect: 'https://www.google.com/maps/place/?q=place_id:',
-  apiUrl: 'http://localhost:5000/api/place'
+  apiUrl: 'http://localhost:5000/api/place',
+  firebaseConfig: {
+    apiKey: "AIzaSyCA2gEb2e7o5_yJD7wJpVkkNR3N06mguCM",
+    authDomain: "storelocation-f9311.firebaseapp.com",
+    databaseURL: "https://storelocation-f9311.firebaseio.com",
+    projectId: "storelocation-f9311",
+    storageBucket: "storelocation-f9311.appspot.com",
+    messagingSenderId: "700070339454",
+    appId: "1:700070339454:web:5509124221c6b1248ec92f",
+    measurementId: "G-WM3JSYF0G8"
+  }
 };
 
 /*
